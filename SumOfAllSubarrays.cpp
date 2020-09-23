c63cc0e9568810a302e3c7e3e7f83f3e4f42925e
Prerna <prerna1998.ps@gmail.com>
2020-09-24 05:09:20 +0530

    Sum of all subarrays of an array using Formula (Efficient)

--- SumOfAllSubarrays.cpp
+++ SumOfAllSubarrays.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int print_subarray(vector<int>, int=0, int=0, int=0);
+int sum_using_formula(vector<int>);
 
 int main(){
     vector<int> array ={1,2,3,4};
     cout<<"$\n";
     cout<<print_subarray(array);
+
+    cout<<"\n\nUsing Formula: "<<sum_using_formula(array); //array[i]*(n-i)(i+1)
     return 0;
 }
 
@@ -27,4 +30,12 @@ int print_subarray(vector<int> array, int start, int end, int sum){
         cout<<"]\n";
         return print_subarray(array, start, end+1, sum);
     }
+}
+
+int sum_using_formula(vector<int> array){
+    int sum=0;
+    for(int i=0;i<array.size(); i++){
+        sum+= (array[i] * (array.size()-i) * (i+1));
+    }
+    return sum;
 }
